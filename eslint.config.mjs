c3c36d9ed8ad901500eb15f3f0a0a8163eec76ed
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-02-25 19:01:22 -0800

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,30 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    "ignores": [
+      "components/ui/*",
+      "lib/utils.ts"
+    ],
+    "rules": {
+      "indent": [
+        "error",
+        2,
+        {
+          "SwitchCase": 1
+        }
+      ],
+      "linebreak-style": ["error", "unix"],
+      "quotes": ["error", "double"],
+      "semi": ["error", "always"],
+      "no-console": 0,
+      "@typescript-eslint/no-unused-vars": ["warn", {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }]
+    }
+  }
 ];
 
 export default eslintConfig;
