c7a2a08c11b3b823489ebe8e55976d27532368c1
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-02-27 12:24:08 -0800

    update section title text color

--- src/layouts/section-layout.tsx
+++ src/layouts/section-layout.tsx
@@ -1,7 +1,7 @@
 const SectionLayout = ({ title, children }: { title: string, children: React.ReactNode }) => {
   return (
     <div className="w-full flex flex-col gap-4 py-4 md:py-4">
-      <h2 className="w-full text-left text-3xl md:text-5xl border-b pb-2">{title}</h2>
+      <h2 className="w-full text-left text-3xl md:text-5xl border-b pb-2 text-text-2">{title}</h2>
       {children}
     </div>
   );
